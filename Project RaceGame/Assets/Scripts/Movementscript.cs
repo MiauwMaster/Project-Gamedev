c29a9ba5e78b8d6c98a50be5aa575e0d6d996763
MiauwMaster <putm1500@student.nhl.nl>
2016-10-25 11:27:45 +0200

    versie 0.2.1
    script update

--- Project RaceGame/Assets/Scripts/Movementscript.cs
+++ Project RaceGame/Assets/Scripts/Movementscript.cs
@@ -1,36 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movementscript : MonoBehaviour {
+public class Movementscript : MonoBehaviour
+{
 
-	
-    Rigidbody2D rigidbody2d;
-    public float speed = 2000F;
-    public float backwspeed = 1800F;
-    public float turnSpeed = 2;
+
+    public float power = 3;
+    public float maxspeed = 5;
+    public float turnpower = 2;
+    public float friction = 3;
+    public Vector2 curspeed;
     public Rigidbody2D rb;
-    // Use this for initialization
-	void Start () {
-        GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (Input.GetKey(KeyCode.W)) {
-            rb.AddForce(transform.forward * speed * Time.deltaTime);
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        curspeed = new Vector2(rb.velocity.x, rb.velocity.y);
+
+        if (curspeed.magnitude > maxspeed)
+        {
+            curspeed = curspeed.normalized;
+            curspeed *= maxspeed;
         }
-        if (Input.GetKey(KeyCode.S)) {
-            rb.AddForce(-(transform.forward) * backwspeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.up * power);
+            rb.drag = friction;
         }
-        if (Input.GetKey(KeyCode.D)) {
-            //rb.transform.Rotate(0, 0, 2);
-            rb.transform.Rotate(Vector3.forward * turnSpeed);
-            
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-(transform.up) * (power / 2));
+            rb.drag = friction;
         }
-        if (Input.GetKey(KeyCode.A)) { 
-            //rb.transform.Rotate(0, 0, -2);
-            rb.transform.Rotate(Vector3.forward * -turnSpeed);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * turnpower);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -turnpower);
+        }
+
+        noGas();
+
+    }
+
+    void noGas()
+    {
+        bool gas;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            gas = true;
+        }
+        else
+        {
+            gas = false;
+        }
+
+        if (!gas)
+        {
+            rb.drag = friction * 2;
         }
     }
 }
