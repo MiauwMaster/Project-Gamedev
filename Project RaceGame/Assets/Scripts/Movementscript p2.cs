6579ef83314ca24aa2926ff5ee27ad48d9f9ae05
Nikkes <nick.westerdijk@gmail.com>
2016-10-25 11:41:32 +0200

    Versie 0.2.2
    Added movement for p2

---
+++ Project RaceGame/Assets/Scripts/Movementscript p2.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movementscript_p2 : MonoBehaviour
+{
+
+
+    public float power = 3;
+    public float maxspeed = 5;
+    public float turnpower = 2;
+    public float friction = 3;
+    public Vector2 curspeed;
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        curspeed = new Vector2(rb.velocity.x, rb.velocity.y);
+
+        if (curspeed.magnitude > maxspeed)
+        {
+            curspeed = curspeed.normalized;
+            curspeed *= maxspeed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * power);
+            rb.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(-(transform.up) * (power / 2));
+            rb.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * turnpower);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -turnpower);
+        }
+
+        noGas();
+
+    }
+
+    void noGas()
+    {
+        bool gas;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            gas = true;
+        }
+        else
+        {
+            gas = false;
+        }
+
+        if (!gas)
+        {
+            rb.drag = friction * 2;
+        }
+    }
+}
