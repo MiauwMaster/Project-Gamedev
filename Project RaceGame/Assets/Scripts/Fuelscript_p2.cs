183abe56e4e3f120416273748e00a09bf26442c4
Nikkes <nick.westerdijk@gmail.com>
2016-10-26 11:58:05 +0200

    0.2.9
    Added fuel scripts + adjustments in scripts

---
+++ Project RaceGame/Assets/Scripts/Fuelscript_p2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fuelscript_p2 : MonoBehaviour
+{
+
+    public float fuelcount;
+    public float fuelusage;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            fuelcount = fuelcount - fuelusage * Time.deltaTime;
+        }
+
+        if (fuelcount <= 0)
+        {
+            Movementscript_p2.power = 0;
+            fuelcount = 0;
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 100, 0, 100, 50), "Fuel: " + fuelcount);
+
+    }
+
+
+}
