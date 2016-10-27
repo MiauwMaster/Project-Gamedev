75bf1b91ac0d75f3d428032d4302f2355ca23ba0
Nikkes <nick.westerdijk@gmail.com>
2016-10-27 13:08:21 +0200

    Versie 0.3.5
    Sand drag added

---
+++ Project RaceGame/Assets/Scripts/Sand.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sand : MonoBehaviour
+{
+
+    public int dragamount;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "player1")
+        {
+            Movementscript.friction = dragamount;
+        }
+
+        if (other.gameObject.tag == "player2")
+        {
+            Movementscript_p2.friction = dragamount;
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "player1")
+        {
+            Movementscript.friction = 2;
+        }
+
+        if (other.gameObject.tag == "player2")
+        {
+            Movementscript_p2.friction = 2;
+        }
+    }
+
+}
