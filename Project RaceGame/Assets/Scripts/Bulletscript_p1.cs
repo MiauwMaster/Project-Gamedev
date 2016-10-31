fa92cbb4d3558802cff29b64763d94face1835fb
Nikkes <nick.westerdijk@gmail.com>
2016-10-31 12:31:19 +0100

    0.4.3
    Added bulletscripts p1, p2

---
+++ Project RaceGame/Assets/Scripts/Bulletscript_p1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;   
+
+public class Bulletscript_p1 : MonoBehaviour {
+
+    GameObject Bulletprefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Firebullet();
+        }
+	
+	}
+
+    public void Firebullet()
+    {
+        GameObject Clone;
+
+        Clone = (Instantiate(Bulletprefab, transform.position, transform.rotation)) as GameObject;
+
+        Clone.GetComponent<Rigidbody>().AddForce(1000, 0, 0);
+    }
+}
